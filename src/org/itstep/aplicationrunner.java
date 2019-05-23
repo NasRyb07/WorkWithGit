215bf5103e69be6cec04878651ccfa61bfdad617
NasRyb07 <Nastya_07@bigmir.net>
2019-05-23 19:41:01 +0300

    Initial commit

---
+++ src/org/itstep/aplicationrunner.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+public class aplicationrunner {
+
+    public static void main(String[] args) {
+        someMethod("Hi? i'm Nastya");
+    }
+
+    static void someMethod(String msg){
+        System.out.println(msg);
+    }
+}
+
+
